6f74d99e766020c4523f3e5fe8b394e92e52281c
israel <lisrael1989@gmail.com>
2024-03-27 21:43:30 +0200

    change img

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom'
 import React from 'react'
-// import logoImg from '../assets/Img/doll.png'
-import logoImg from '../assets/Img/hero5.webp'
+
+import logoImg from '../assets/Img/train.png'
 
 
 export function HomePage() {
 
   return (
     <section className='home-page home-preview flex '>
-      <h1 className="home-page-slogan">Where Playtime Never Ends!</h1>
+      <h1 className="home-page-slogan">Where  <br /><span>Play time</span> Never Ends!</h1>
       <Link to="/toy"><img className=" bouncing-toy hero-img" src={logoImg} />
       </Link>
     </section >
